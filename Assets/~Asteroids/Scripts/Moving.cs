1aac9023ceef9e9151b5fe0190378f62c3d7afa3
oscar.pugh1 <oscar.pugh1@600900J211DE415>
2018-02-23 09:28:09 +1100

    Added Unity Project

---
+++ Assets/~Asteroids/Scripts/Moving.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Clean Code: CTRL + K + D (in that order)
+
+    // {} - Braces
+    // [] - Brackets
+    // () - Parentesis
+
+public class Moving : MonoBehaviour
+{
+    public float rotationSpeed = 360;
+    public float movementSpeed = 10;
+    public float movementSpeedback = 5;
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    transform.Translate(Vector3.up * movementSpeed * Time.deltaTime);
+                }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    transform.Translate(Vector3.down * movementSpeedback * Time.deltaTime);
+                }
+
+     
+    }
+
+    void Rotation()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    transform.Rotate(Vector3.back, rotationSpeed * Time.deltaTime);
+                }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+                }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Rotation();
+    }
+}
