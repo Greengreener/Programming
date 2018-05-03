b4245cc440a72d7c11c15ca8640c9052e5a56845
oscar.pugh1 <oscar.pugh1@600900J211DE415>
2018-05-04 09:35:10 +1000

    Add a new .gitignore

---
+++ Unity/Assets/~Asteroids/Scripts/Moving.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Clean Code: CTRL + K + D (in that order)
+
+// {} - Braces
+// [] - Brackets
+// () - Parentesis
+
+namespace Asteroids
+{
+    public class Moving : MonoBehaviour
+    {
+        public float rotationSpeed = 360;
+        public float movementSpeed = 2;
+        public float movementSpeedback = 2;
+
+        void Movement()
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.up * movementSpeed * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Vector3.down * movementSpeedback * Time.deltaTime);
+            }
+
+
+        }
+
+        void Rotation()
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(Vector3.back, rotationSpeed * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Movement();
+            Rotation();
+        }
+    }
+}
