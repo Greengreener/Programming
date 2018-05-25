96a268143b7b38b17e38e7b5e71338a5f74d2162
oscar.pugh1 <oscar.pugh1@600900J211DE415>
2018-05-25 10:03:58 +1000

    Finished the tutorials

--- Unity/Assets/~Breakout/Scripts/Ball.cs
+++ Unity/Assets/~Breakout/Scripts/Ball.cs
@@ -14,6 +14,7 @@ namespace Breakout
         public void Fire(Vector3 direction)
         {
             velocity = direction * speed;
+
         }
 
         void OnCollisionEnter2D(Collision2D other)
